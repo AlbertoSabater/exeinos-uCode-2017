8cbb4555f7c6a077ed51f4a17fd2f7c704701595
Antonio Martínez Garín <amgarin90@gmail.com>
2017-03-11 15:08:39 +0100

    Reconocedor de habla con opciones de ejemplo
    -Colores, tamaño, modelos, precio

--- app/src/main/java/com/example/android/photobyintent/OptionsActivity.java
+++ app/src/main/java/com/example/android/photobyintent/OptionsActivity.java
@@ -3,12 +3,19 @@ package com.example.android.photobyintent;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Antonio on 11/3/17.
  */
 public class OptionsActivity extends Activity {
+
+    private ListView wordsList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +23,13 @@ public class OptionsActivity extends Activity {
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
-        String message = intent.getStringExtra(PhotoIntentActivity.EXTRA_MESSAGE);
+        ArrayList<String> matches = intent.getStringArrayListExtra(PhotoIntentActivity.EXTRA_MESSAGE);
 
         // Capture the layout's TextView and set the string as its text
-        TextView textView = (TextView) findViewById(R.id.optionText);
-        textView.setText(message);
+//        TextView textView = (TextView) findViewById(R.id.optionText);
+//        textView.setText(message);
+
+        wordsList = (ListView) findViewById(R.id.list);
+        wordsList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, matches));
     }
 }
